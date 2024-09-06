f0f2866265acc4b69ddbe79bf8a1ec36ca1e8ac0
Sara Nurminen <sara.nurminen@tuni.fi>
2024-09-06 13:40:38 +0300

    missing 2

--- wk02_decrease_or_divide/missing/missing2.cc
+++ wk02_decrease_or_divide/missing/missing2.cc
@@ -3,8 +3,33 @@
 #include "test.hh"
 
 int searchSmallestMissing(int* A, int left, int right){
-    //TODO: your code here
-    //return missing value OR
-    //return NO_VALUE_MISSING if no value is missing
-    return NO_VALUE_MISSING;
+    /// triviaalitapaus, eli vasen ja oikea osoittavat samaan soluun
+    if (left == right) {
+        /// katsotaan onko arvo indeksin mukaan oikeassa paikassa
+        if (A[left] == A[0]+left) {
+            /// Mitään ei puutu jos indeksi on oikein
+            return NO_VALUE_MISSING;
+
+        /// Jos taas left ja right osoittivat samaan paikkaan, mutta
+        /// indeksin arvo ja indeksi olivat vituillaan, tämä on puuttuva arvo
+        } else {
+            return A[0]+left;
+        }
+
+    }
+    else {
+       /// tarkastetaan onko keskikohta oikeassa paikassa
+       int mid = left + (left+right)/2;
+
+       /// jos keskikohta on väärässä paikassa, etsittävä arvo
+       /// on vasemmassa puolikkaassa
+       if (A[mid] != A[0]+mid) {
+           return searchSmallestMissing(A, left, mid);
+       }
+       else {
+           /// jos keskikohta on oikein, etsitään oikeasta puolikkaasta
+           return searchSmallestMissing(A, mid+1, right);
+       }
+    }
+
 }
