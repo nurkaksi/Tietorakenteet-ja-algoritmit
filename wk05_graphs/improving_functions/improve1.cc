21d5446d72bd6703d0c7c2b745201f83c446416c
Sara Nurminen <sara.nurminen@tuni.fi>
2024-09-25 18:22:40 +0300

    ascending vector

--- wk05_graphs/improving_functions/improve1.cc
+++ wk05_graphs/improving_functions/improve1.cc
@@ -1,6 +1,7 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
 
 /**
@@ -10,9 +11,10 @@
  * @return std::vector<int>
  */
 std::vector<int> ascendingVector(int n){
-    std::vector<int> v;
-    for ( int i = 0; i < n ; i++){
-        v.insert(v.end(), i);
-    }
+    std::vector<int> v(n);
+
+    // varataan valmiiksi riittävästi muistia vektoria varten
+    v.reserve(n);
+    std::iota(v.begin(), v.end(), 0);
     return v;
 }
